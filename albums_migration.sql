19c98b931fe222e809034e0899d86dacec0631c6
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-04 15:59:39 -0500

    working on albums_seeder.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,8 @@
 USE codeup_test_db;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) NOT NULL, name VARCHAR(100) NOT NULL,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     release_date INT UNSIGNED DEFAULT NULL,
     sales FLOAT,
     genre VARCHAR(255),
