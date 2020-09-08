c1514b83a9d55d802fd2327c7c40a7fd7dd36d0f
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-08 12:15:26 -0500

    where_exercises finished

--- where_exercises.sql
+++ where_exercises.sql
@@ -15,3 +15,45 @@ WHERE birth_date LIKE '%12-25';
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+  AND (
+        gender = 'M'
+    )
+   OR first_name = 'Vidya'
+    AND (
+              gender = 'M'
+          )
+   OR first_name = 'Maya'
+    AND (
+        gender = 'M'
+          );
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND (
+        last_name NOT LIKE '%qu%');
