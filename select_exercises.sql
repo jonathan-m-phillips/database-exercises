4a3e35040e6bac69ab7968103f57f3b9d99b11b6
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-08 11:12:55 -0500

    updated exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,19 +1,19 @@
 USE codeup_test_db;
 
-/* Album names of all Pink Floyd Albums */
+SELECT 'All Albums from Pink Floyd' AS 'Info';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
-/* Release date of Beatles album */
+SELECT 'Release date of Sgt. Pepper''s Lonely Hearts Club Band' AS 'Info';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-/* Genre of Nirvana Nevermind album */
+SELECT 'Genre of Nirvana Nevermind album' AS 'Info';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
-/* All Albums released in 1990s */
+SELECT 'Albums of the 90''s' AS 'Info';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-/* ALL Albums less than 20 million certified sales */
+SELECT 'Albums under 20 million certified sales' AS 'Info';
 SELECT name FROM albums WHERE sales < 20.0;
 
-/* All Albums with genre of rock */
+SELECT 'Albums in the Rock genre' AS 'Info';
 SELECT name FROM albums WHERE genre = 'Rock';
