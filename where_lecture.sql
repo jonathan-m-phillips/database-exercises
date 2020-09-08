7bc424c1f11d6b4519c256dfe39ff781fd939ff4
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-08 11:51:21 -0500

    parenthesis to and or properly

--- where_lecture.sql
+++ where_lecture.sql
@@ -18,3 +18,11 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name IN ('Herber', 'Baek')
     AND emp_no < 20000;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no < 20000
+    AND (
+        last_name IN ('Herber', 'Baek')
+        OR first_name = 'Shridhar'
+    );
